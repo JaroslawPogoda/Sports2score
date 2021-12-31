5dd652c0e564ded832f5b986e5a284c5b09f5a00
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-30 23:59:55 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // //import fetch from "node-fetch";
 
-
-
 // fetch("https://api.the-odds-api.com/v4/sports?apiKey=3d76b18d0834a940c9f7f2511cd08450")
 // .then(response => {
 // 	console.log(response);
@@ -9,7 +7,6 @@
 // .catch(err => {
 // 	console.error(err);
 // });
-
 
 // fetch("https://football-pro.p.rapidapi.com/api/v2.0/tvstations/fixture/11867339?tz=Europe%2FAmsterdam", {
 // 	"method": "GET",
@@ -50,7 +47,7 @@
 //   method: 'GET',
 //   headers: myHeaders,
 //   redirect: 'follow',
-  
+
 // };
 // let createTableTH = heading => {
 //   console.log(heading)
@@ -66,52 +63,116 @@
 //       fetch(`https://soccer.sportmonks.com/api/v2.0/seasons/:${lastSeason.id}?api_token=${API_TOKEN}`)
 //     .then(response =>response.json())
 //     .then(data =>console.log(data) )})
-    
-
 
 //   return
 // }
 // fetch(`https://soccer.sportmonks.com/api/v2.0/leagues?api_token=${API_TOKEN}`, requestOptions)
 //   .then(response => response.json())
-//   .then(result => 
+//   .then(result =>
 //     {
 //       // console.log(result.data)
 //       for (let n = 0;n< result.data.length;n++){
 //         returnData(result.data[n])
 //         createTableTH(result.data[n])}})
-        
+
 //   .catch(error => console.log('error', error));
 //
 // import * from '../node_modules'
-let addToTable = data=>{ let table = document.querySelector('.table_league')
-table.innerHTML +=`<tr><th>${data.rank}</th><th>${data.name}</th> <th>${data.matches}</th><th>${data.points}</th> <th>${data.won}</th> <th>${data.lost}</th><th>${data.drawn}</th><th>${data.goal_diff}</th> <th>${data.goals_conceded}</th> <th>${data.goals_scored}</th></tr>`}
-fetch("https://live-score-api.p.rapidapi.com/leagues/table.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&&season=8&competition_id=2", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "live-score-api.p.rapidapi.com",
-		"x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b"
+let arrowButton = document.querySelector(".thatarrow")
+let table = document.querySelector(".table_league");
+let leagueTitle = document.getElementById("league");
+let selectDoc =document.querySelector("#leagues");
+let currentCompetition = 2;
+let competitions = 0;
+let lookfor = "";
+let ran=true;
+function compare( a, b ) {
+	if ( a.id < b.id ){
+	  return -1;
 	}
-})
-.then(response => response.json())
-.then(data => {
-  console.log()
-for(let i = 0; i < data.data.table.length; i++) {
-  console.log(data.data.table[i])
-  addToTable(data.data.table[i])
-}  
-})
-.catch(err => {
-	console.error(err);
-});
-fetch("https://live-score-api.p.rapidapi.com/competitions/list.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&&season=12&competition_id=2", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "live-score-api.p.rapidapi.com",
-		"x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b"
+	if ( a.id > b.id ){
+	  return 1;
 	}
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => {
-	console.error(err);
-});
+	return 0;
+  }
+let addToTable = (data) => {
+  
+  table.innerHTML += `<tr><td>${data.rank}</td><td>${data.name}</td> <td>${data.matches}</td><td>${data.points}</td> <td>${data.won}</td> <td>${data.lost}</td><td>${data.drawn}</td><td>${data.goal_diff}</td> <td>${data.goals_conceded}</td> <td>${data.goals_scored}</td></tr>`;
+};
+function getCompetitonsName() {
+	
+  fetch(
+    `https://live-score-api.p.rapidapi.com/competitions/list.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&&season=12&competition_id=${currentCompetition}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "live-score-api.p.rapidapi.com",
+        "x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+		
+      let league = "";
+      
+      competitions = data.data;
+	  if(competitions==0)
+	  console.log('this shit')
+      league = competitions.competition.find(
+        (element) => element.id == lookfor
+      );
+      console.log(competitions)
+	  leagueTitle.innerText += " " + league.name;
+      competitions.conpetition = competitions.competition.sort(compare)
+	  if(ran){
+	  competitions.competition.map(element=>{
+		  if(element.countries[0]){
+			selectDoc.innerHTML+=`<option value="${element.id}">${element.name} ${element.countries[0].name}</option>`
+		  }
+		  else{
+			selectDoc.innerHTML+=`<option value="${element.id}">${element.name}</option>`
+		  }
+	  })
+		ran=false
+	} 
+	  //console.log(selectDoc.innerHTML)
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+	function getDatafromApi(competition_id) {
+		currentCompetition = competition_id;
+  fetch(
+    `https://live-score-api.p.rapidapi.com/leagues/table.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&&season=13&competition_id=${currentCompetition}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "live-score-api.p.rapidapi.com",
+        "x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      lookfor = data.data.table[0].competition_id;
+	  table.innerHTML ='<table class="table_league"><tr><th>rank:</th><th>name:</th> <th>matches:</th><th>points:</th> <th>won:</th> <th>lost:</th><th>drawn:</th><th>goal_diff:</th> <th>goals_conceded:</th> <th>goals_scored:</th></tr></table></tr></table>'
+	  leagueTitle.innerText = "The league" 
+	  for (let i = 0; i < data.data.table.length; i++) {
+        addToTable(data.data.table[i]);
+      }
+	  getCompetitonsName();
+	  
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+getDatafromApi(2);
+arrowButton.addEventListener("click",()=>getDatafromApi(++currentCompetition))
+selectDoc.addEventListener("change",()=>{
+	console.log(selectDoc.value)
+	return getDatafromApi(Number(selectDoc.value))})
+console.log(competitions); //.find(element = element.competition_id)
