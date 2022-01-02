d2697533a1bcf0c98c5b60a21b3bd639375ce002
Jaroslaw Pogoda <jarek1330@gmail.com>
2022-01-02 16:12:18 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -48,11 +48,10 @@ function getCompetitonsName() {
       let league = "";
 
       competitions = data.data;
-      if (competitions == 0) console.log("this shit");
+      if (competitions == 0) console.log("this no competition");
       league = competitions.competition.find(
         (element) => element.id == lookfor
       );
-      console.log(competitions);
       leagueTitle.innerText += " " + league.name;
 
       competitions.competition = competitions.competition.sort(compare);
@@ -77,19 +76,15 @@ function getCompetitonsName() {
 }
 
 function getDatafromApi(competition_id) {
-  console.log(competition_id);
   currentCompetition = competition_id;
   if (!ran) {
-    console.log(
       competitions.competition.filter(
         (element) => element.id == currentCompetition
       )
-    );
     currentcompetitionlookup = competitions.competition.filter(
       (element) => element.id == currentCompetition
     );
   }
-  console.log(currentcompetitionlookup[0].season.id);
   fetch(
     `https://live-score-api.p.rapidapi.com/leagues/table.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&&season=${currentcompetitionlookup[0].season.id}&competition_id=${currentCompetition}`,
     {
@@ -104,14 +99,12 @@ function getDatafromApi(competition_id) {
     .then((data) => {
       if (data.data.table[0]) {
         lookfor = data.data.table[0].competition_id;
-
         table.innerHTML =
           '<table class="table_league"><tr><th>rank:</th><th>name:</th> <th>matches:</th><th>points:</th> <th>won:</th> <th>lost:</th><th>drawn:</th><th>goal_diff:</th> <th>goals_conceded:</th> <th>goals_scored:</th></tr></table></tr></table>';
         leagueTitle.innerText = "The league";
         for (let i = 0; i < data.data.table.length; i++) {
           addToTable(data.data.table[i]);
         }
-
         getCompetitonsName();
       }
     })
@@ -121,25 +114,22 @@ function getDatafromApi(competition_id) {
 }
 getDatafromApi(2);
 arrowButtonRight.addEventListener("click", () => {
-  console.log(
+
     competitions.competition.find((element, index) => {
       currentindex = index;
       return element.id == currentCompetition;
     })
-  ); //.indexof(competitions.competition.find(element=>element.id==currentCompetition)))
-  console.log(currentindex);
+ //.indexof(competitions.competition.find(element=>element.id==currentCompetition)))
   currentCompetition = Number(competitions.competition[currentindex].id);
   return getDatafromApi(competitions.competition[++currentindex].id);
 });
 
 arrowButtonLeft.addEventListener("click", () => {
-  console.log(
     competitions.competition.find((element, index) => {
       currentindex = index;
       return element.id == currentCompetition;
     })
-  ); //.indexof(competitions.competition.find(element=>element.id==currentCompetition)))
-  console.log(currentindex);
+   //.indexof(competitions.competition.find(element=>element.id==currentCompetition)))
   currentCompetition = Number(competitions.competition[currentindex].id);
   return getDatafromApi(competitions.competition[--currentindex].id);
 });
@@ -147,82 +137,137 @@ arrowButtonLeft.addEventListener("click", () => {
 selectDoc.addEventListener("change", () => {
   currentCompetition = selectDoc.value;
   return getDatafromApi(Number(currentCompetition));
-});
-console.log(competitions); //.find(element = element.competition_id)
-function getFixturesForTeam(idOfTeam){
-fetch(`https://live-score-api.p.rapidapi.com/fixtures/matches.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&team=${idOfTeam}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "live-score-api.p.rapidapi.com",
-		"x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b"
-	}
-})
-.then(response => response.json())
-.then(data=>{
-  let tdFixturesDoc =document.getElementById(`fixtures${idOfTeam}`)
-  let tdFixturesDoc2 =document.querySelector(`.match${idOfTeam}`)
-  if(tdFixturesDoc2!=null && tdFixturesDoc2.style.display=="none"){
-    console.log("inside if")
-    tdFixturesDoc2.style.display="table-cell"
-}
-else if(tdFixturesDoc2!=null){
-  console.log("inside else if")
-  tdFixturesDoc2.style.display="none"
-}
-else{
-  console.log(tdFixturesDoc2)
-  console.log("inside else")
-  tdFixturesDoc.innerHTML=`<div class="container2">
-  <ion-icon class="arrow3 thatarrow3" name="chevron-forward-outline" ></ion-icon>
-
+}); //.find(element = element.competition_id)
+function getFixturesForTeam(idOfTeam, index = 0) {
+  fetch(
+    `https://live-score-api.p.rapidapi.com/fixtures/matches.json?secret=obxADujFSHjoiWyc1eJ6uRnuvg3NFph3&key=T5Fx5g8gZESziplt&team=${idOfTeam}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "live-score-api.p.rapidapi.com",
+        "x-rapidapi-key": "ac81564003mshac18851ef49a542p1dc604jsn3f0f121e7c3b",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let tdFixturesDoc = document.getElementById(`fixtures${idOfTeam}`);
+      let tdFixturesDoc2 = document.querySelector(`.match${idOfTeam}`);
+      if (tdFixturesDoc2 != null && tdFixturesDoc2.style.display == "none") {
+        tdFixturesDoc2.style.display = "table-cell";
+        document.querySelector(`.arrowRight${idOfTeam}`).style.display =
+          "table-cell";
+        document.querySelector(`.arrowLeft${idOfTeam}`).style.display =
+          "table-cell";
+      } else if (tdFixturesDoc2 != null) {
+        tdFixturesDoc2.style.display = "none";
+        document.querySelector(`.arrowRight${idOfTeam}`).style.display = "none";
+        document.querySelector(`.arrowLeft${idOfTeam}`).style.display = "none";
+      } else {
+        tdFixturesDoc.innerHTML = `<div class="container2">
+  <ion-icon class="arrow4 thatarrow4 arrowLeft${idOfTeam}" name="chevron-back-outline" ></ion-icon>
 	<div class="match match${idOfTeam}">
 		<div class="match-header">
-			<div class="match-tournament"><img src="" />${data.data.fixtures[0].competition.name}</div>
+			<div class="match-tournament${idOfTeam}">${data.data.fixtures[index].competition.name}</div>
       <div></div>
 		</div>
 		<div class="match-content">
 			<div class="column">
-				<div class="team team--home">
-					<h2 class="team-name">${data.data.fixtures[0].home_name}</h2>
+				<div class="team team--home${idOfTeam}">
+					<h2 class="team-nameh${idOfTeam}">${data.data.fixtures[index].home_name}</h2>
 				</div>
 			</div>
 			<div class="column">
 				<div class="match-details">
-					<div class="match-date">
-						${data.data.fixtures[0].date} at <strong>${data.data.fixtures[0].time}</strong>
+					<div class="match-date${idOfTeam}">
+						${data.data.fixtures[index].date} at <strong>${data.data.fixtures[index].time}</strong>
 					</div>
 					<div class="match-score">
 						<span class="match-score-divider">vs.</span>
 					</div>
-					<div class="match-referee">
-						Location: <strong>${data.data.fixtures[0].location}</strong>
+					<div class="match-referee${idOfTeam}">
+						Location: <strong>${data.data.fixtures[index].location}</strong>
 					</div>
 					<div class="match-bet-options">
-						<button class="match-bet-option">${data.data.fixtures[0].odds.live[1]}</button>
-						<button class="match-bet-option">${data.data.fixtures[0].odds.live[2]}</button>
-						<button class="match-bet-option">${data.data.fixtures[0].odds.live.X}</button>
+						<button class="match-bet-option match-bet-option1${idOfTeam}">${data.data.fixtures[index].odds.live[1]}</button>
+						<button class="match-bet-option match-bet-option2${idOfTeam}">${data.data.fixtures[index].odds.live[2]}</button>
+						<button class="match-bet-option match-bet-option3${idOfTeam}">${data.data.fixtures[index].odds.live.X}</button>
 					</div>
 				</div>
 			</div>
 			<div class="column">
 				<div class="team team--away">
-					<h2 class="team-name"> ${data.data.fixtures[0].away_name}</h2>
+					<h2 class="team-namea${idOfTeam}"> ${data.data.fixtures[index].away_name}</h2>
 				</div>
 			</div>
 		</div>
 	</div>
+  <div></div>
+  <ion-icon class="arrow3 thatarrow3 arrowRight${idOfTeam}"  name="chevron-forward-outline" ></ion-icon>
 </div>
-<div>
-  <div>
-    <div></div>
-  </div>
-  <ion-icon class="arrow3 thatarrow4" name="chevron-back-outline" ></ion-icon>
-</div>
-`
-}
-  console.log(data.data.fixtures)
-  console.log(data.data)})
-.catch(err => {
-	console.error(err);
-});
+`;
+        //onclick="getNextFixture(${data.data.fixtures[(index+1)]},${index},${idOfTeam})"
+        let arrowRightTag = document.querySelector(`.arrowRight${idOfTeam}`);
+        arrowRightTag.addEventListener("click", () => {
+          if (index >= 0 && ++index < data.data.fixtures.length) {
+            document.querySelector(`.match-tournament${idOfTeam}`).innerText =
+              data.data.fixtures[index].competition.name;
+            document.getElementsByClassName(
+              `team-nameh${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].home_name;
+            document.getElementsByClassName(
+              `match-date${idOfTeam}`
+            )[0].innerHTML = `${data.data.fixtures[index].date} at <strong>${data.data.fixtures[index].time}</strong>`;
+            document.getElementsByClassName(
+              `match-referee${idOfTeam}`
+            )[0].innerHTML = `Location: <strong>${data.data.fixtures[index].location}</strong>`;
+            document.getElementsByClassName(
+              `match-bet-option1${idOfTeam}`
+            )[0].InnerHTML = data.data.fixtures[index].odds.live[1];
+            document.getElementsByClassName(
+              `match-bet-option2${idOfTeam}`
+            )[0].innerHTML = data.data.fixtures[index].odds.live[2];
+            document.getElementsByClassName(
+              `match-bet-option3${idOfTeam}`
+            )[0].innerHTML = data.data.fixtures[index].odds.live.X;
+            document.getElementsByClassName(
+              `team-namea${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].away_name;
+          }
+          // ;
+        });
+        let arrowLeftTag = document.querySelector(`.arrowLeft${idOfTeam}`);
+        arrowLeftTag.addEventListener("click", () => {
+          if (--index >= 0 && index < data.data.fixtures.length) {
+            document.querySelector(`.match-tournament${idOfTeam}`).innerText =
+              data.data.fixtures[index].competition.name;
+            document.getElementsByClassName(
+              `team-namea${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].home_name;
+            document.getElementsByClassName(
+              `match-date${idOfTeam}`
+            )[0].innerHTML = `${data.data.fixtures[index].date} at <strong>${data.data.fixtures[index].time}</strong>`;
+            document.getElementsByClassName(
+              `match-referee${idOfTeam}`
+            )[0].innerHTML = `Location: <strong>${data.data.fixtures[index].location}</strong>`;
+            document.getElementsByClassName(
+              `match-bet-option1${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].odds.live[1];
+            document.getElementsByClassName(
+              `match-bet-option2${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].odds.live[2];
+            document.getElementsByClassName(
+              `match-bet-option3${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].odds.live.X;
+            document.getElementsByClassName(
+              `team-nameh${idOfTeam}`
+            )[0].innerText = data.data.fixtures[index].away_name;
+            // ;
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
